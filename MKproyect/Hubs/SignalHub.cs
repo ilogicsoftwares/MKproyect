fdcf4c2e73f52e5c4aff650ae65a8aa4e2d6e5b8
Richard Aguirre <richardjap@gmail.com>
2018-11-30 12:30:59 -0500

    Initial commit

---
+++ MKproyect/Hubs/SignalHub.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SignalRChatServer.Hubs
+{
+    public class SignalHub:Hub
+    {
+       
+        
+        public  Task JoinMachineChanel (string machineID)
+        {
+            return Groups.Add(Context.ConnectionId, machineID);
+
+        }
+
+
+        public Task LeaveMachineChanel(string machineID)
+        {
+            return Groups.Remove(Context.ConnectionId, machineID);
+        }
+        public override Task OnConnected()
+        {
+            var con = Context.ConnectionId;
+            return base.OnConnected();
+        }
+
+       
+        public void SendData (string machineID, String message)
+        {
+
+         
+            Clients.Group(machineID).broadCastMessage(message);
+
+        }
+      
+
+       
+
+    }
+
+
+   
+}
